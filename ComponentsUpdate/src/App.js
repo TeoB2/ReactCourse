2913a07957f75968de88cc2eccfe7129c3df1d22
TeoB2 <matteo.belletti@outlook.com>
2021-11-09 11:15:06 +0100

    Use useCallback and React.memo()

--- ComponentsUpdate/src/App.js
+++ ComponentsUpdate/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Button from "./components/UI/Button/Button";
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+
+    if(allowToggle)
+    {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+    
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+
+    setAllowToggle(true);
+  };
+
+  console.log("App running");
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph!</Button>
     </div>
   );
 }
