6fa5c9c1bbc49173357aeac479acf129364b9a40
TeoB2 <matteo.belletti@outlook.com>
2021-11-10 10:38:32 +0100

    Get a Http Request with error managing

--- RequestHttpExercise/src/App.js
+++ RequestHttpExercise/src/App.js
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchMovieHandler()
+  {
+    setIsLoading(true);
+    setError(null);
+
+    try
     {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
+      const response = await fetch("https://swapi.dev/api/films");
+
+      if(!response.ok)
+      {
+        throw new Error('Something went wrong! Error code: ' + response.status);
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseData: movieData.release_date
+        }
+      });
+      setMovies(transformedMovies);
+    }
+    catch(error)
     {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+      setError(error.message);
+    }
+
+    setIsLoading(false);
+  }
+
+  let content = <p>Found no movies</p>;
+
+  if(isLoading)
+  {
+    content = <p>Loading...</p>;
+  }
+  else if(error)
+  {
+    content = <p>{error}</p>;
+  }
+  else if(movies.length > 0)
+  {
+    content = <MoviesList movies={movies} />;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {content}
       </section>
     </React.Fragment>
   );
